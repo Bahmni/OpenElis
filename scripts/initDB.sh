d120cfdbbf0a7cad38366189e90cea5ffa435a9b
Deepak N <endeep123@gmail.com>
2013-07-02 13:02:08 +0530

    #1096 : Changed initDB not to drop database

---
+++ scripts/initDB.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+SCRIPTS_DIR=`dirname $0`
+ROOT_DIR="$SCRIPTS_DIR/.."
+DATABASE_NAME="clinlims"
+set -e
+
+if [ "$(psql -Upostgres -lqt | cut -d \| -f 1 | grep -w $DATABASE_NAME | wc -l)" -eq 0 ]; then
+    echo "Creating database : $DATABASE_NAME"
+    psql -U postgres -f $SCRIPTS_DIR/setupDB.sql
+    pg_restore -U postgres -d $DATABASE_NAME $ROOT_DIR/db_backup/haitiClinical.backup
+else
+    echo "The database $DATABASE_NAME already exits"
+fi
