df6570bea59f0163d8880f79fd3b8c306145c44f
indraneelr <indraneel.rr@gmail.com>
2013-07-08 12:10:36 +0530

    IN | #1069 | build fix attempt 2

--- openelis/test/org/bahmni/feed/openelis/event/OpenelisAtomfeedClientServiceEventWorkerTest.java
+++ openelis/test/org/bahmni/feed/openelis/event/OpenelisAtomfeedClientServiceEventWorkerTest.java
@@ -3,21 +3,21 @@ package org.bahmni.feed.openelis.event;
 import org.ict4h.atomfeed.client.domain.Event;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration(locations = {"classpath*:applicationContext-openelisTest.xml"})
+//@ContextConfiguration(locations = {"classpath*:applicationContext-openelisTest.xml"})
 public class OpenelisAtomfeedClientServiceEventWorkerTest {
     static final String EVENT_CONTENT = " {\"category\": \"panel\", \"list_price\": \"0.0\", \"name\": \"ECHO\", \"type\": \"service\", \"standard_price\": \"0.0\", \"uom_id\": 1, \"uom_po_id\": 1, \"categ_id\": 33, \"id\": 193}";
 
     OpenelisAtomfeedClientServiceEventWorker eventWorker;
     @Before
     public void setUp(){
-        eventWorker = new OpenelisAtomfeedClientServiceEventWorker("hosts/feed/recent");
+   //     eventWorker = new OpenelisAtomfeedClientServiceEventWorker("hosts/feed/recent");
     }
 
     @Test
     public void testProcess() throws Exception {
- //       eventWorker.process(createEvent());
+        eventWorker.process(createEvent());
     }
 
     private Event createEvent(){
