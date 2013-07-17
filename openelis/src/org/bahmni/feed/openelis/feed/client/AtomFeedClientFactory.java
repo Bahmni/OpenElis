c00f105e40361786fc55200420f807c48cbacdb1
pchandra <pchandra@thoughtworks.com>
2013-07-17 21:55:08 +0530

    praveen:fixing compilation errors

--- openelis/src/org/bahmni/feed/openelis/feed/client/AtomFeedClientFactory.java
+++ openelis/src/org/bahmni/feed/openelis/feed/client/AtomFeedClientFactory.java
@@ -2,12 +2,19 @@ package org.bahmni.feed.openelis.feed.client;
 
 import org.bahmni.feed.openelis.utils.OpenElisConnectionProvider;
 import org.ict4h.atomfeed.client.factory.AtomClientFactory;
+import org.ict4h.atomfeed.client.repository.jdbc.AllFailedEventsJdbcImpl;
+import org.ict4h.atomfeed.client.repository.jdbc.AllMarkersJdbcImpl;
 import org.ict4h.atomfeed.client.service.AtomFeedClient;
 import org.ict4h.atomfeed.jdbc.JdbcConnectionProvider;
 
 public class AtomFeedClientFactory {
     static AtomFeedClient getFeedClient() {
         JdbcConnectionProvider jdbcConnectionProvider = new OpenElisConnectionProvider();
-        return new AtomClientFactory().create(jdbcConnectionProvider);
+        AllMarkersJdbcImpl allMarkersJdbc = new AllMarkersJdbcImpl(jdbcConnectionProvider);
+        AllFailedEventsJdbcImpl allFailedEventsJdbc = new AllFailedEventsJdbcImpl(jdbcConnectionProvider);
+
+        return new AtomClientFactory().create(allMarkersJdbc,allFailedEventsJdbc);
     }
+
+
 }
