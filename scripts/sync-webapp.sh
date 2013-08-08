4289db32e83959a3be8334abab865afd601e78b8
kunalch <kunalch@thoughtworks.com>
2013-08-08 12:26:45 +0530

    d3/kunl: sync css, scripts etc

--- scripts/sync-webapp.sh
+++ scripts/sync-webapp.sh
@@ -1,3 +1,9 @@
 scriptdir=$(dirname $0)
-dest='-e ssh root@192.168.33.10:$TOMCAT_HOME/webapps/openelis/'
-sh $scriptdir/rsync-with-options.sh openelis/WebContent/pages $dest
+dest='-e ssh jss@192.168.33.10:/home/jss/apache-tomcat-7.0.22/webapps/openelis'
+sh $scriptdir/rsync-with-options.sh $scriptdir/../openelis/WebContent/css $dest
+sh $scriptdir/rsync-with-options.sh $scriptdir/../openelis/WebContent/images $dest
+sh $scriptdir/rsync-with-options.sh $scriptdir/../openelis/WebContent/pages $dest
+sh $scriptdir/rsync-with-options.sh $scriptdir/../openelis/WebContent/scripts $dest
+
+
+
