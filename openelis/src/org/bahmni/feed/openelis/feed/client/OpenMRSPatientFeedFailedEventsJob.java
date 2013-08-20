2b273be687d9a6842420dca492b088438c761742
pchandra <pchandra@thoughtworks.com>
2013-08-20 06:57:02 +0530

    vivek|moving creation of atom feed out of the constructor

--- openelis/src/org/bahmni/feed/openelis/feed/client/OpenMRSPatientFeedFailedEventsJob.java
+++ openelis/src/org/bahmni/feed/openelis/feed/client/OpenMRSPatientFeedFailedEventsJob.java
@@ -1,6 +1,8 @@
 package org.bahmni.feed.openelis.feed.client;
 
 import org.apache.log4j.Logger;
+import org.bahmni.feed.openelis.AtomFeedProperties;
+import org.ict4h.atomfeed.client.service.AtomFeedClient;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,14 +10,17 @@ import org.quartz.JobExecutionException;
 @DisallowConcurrentExecution
 public class OpenMRSPatientFeedFailedEventsJob extends OpenMRSFeedReaderJob {
     private static Logger logger = Logger.getLogger(OpenMRSPatientFeedFailedEventsJob.class);
+    protected static AtomFeedClient atomFeedClient;
 
-    public OpenMRSPatientFeedFailedEventsJob() {
+    public OpenMRSPatientFeedFailedEventsJob() throws JobExecutionException {
         super(logger);
     }
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
+            if (atomFeedClient == null)
+                atomFeedClient = createAtomFeedClient(AtomFeedProperties.getInstance(), new AtomFeedClientFactory());
             atomFeedClient.processFailedEvents();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
--- openelis/src/org/bahmni/feed/openelis/feed/client/OpenMRSPatientFeedReaderJob.java
+++ openelis/src/org/bahmni/feed/openelis/feed/client/OpenMRSPatientFeedReaderJob.java
@@ -1,6 +1,8 @@
 package org.bahmni.feed.openelis.feed.client;
 
 import org.apache.log4j.Logger;
+import org.bahmni.feed.openelis.AtomFeedProperties;
+import org.ict4h.atomfeed.client.service.AtomFeedClient;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -8,6 +10,7 @@ import org.quartz.JobExecutionException;
 @DisallowConcurrentExecution
 public class OpenMRSPatientFeedReaderJob extends OpenMRSFeedReaderJob {
     private static Logger logger = Logger.getLogger(OpenMRSPatientFeedReaderJob.class);
+    protected static AtomFeedClient atomFeedClient;
 
     public OpenMRSPatientFeedReaderJob() {
         super(logger);
@@ -16,6 +19,8 @@ public class OpenMRSPatientFeedReaderJob extends OpenMRSFeedReaderJob {
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         try {
+            if (atomFeedClient == null)
+                atomFeedClient = createAtomFeedClient(AtomFeedProperties.getInstance(), new AtomFeedClientFactory());
             atomFeedClient.processEvents();
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
