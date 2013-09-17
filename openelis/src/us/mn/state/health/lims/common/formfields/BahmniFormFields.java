243c605855e13eae904a65bfe30cb56eca66ecba
Vinay Venu <vinayv@thoughtworks.com>
2013-09-17 15:06:23 +0530

    Vinay | #0 | Remove Collection date from StatusResults.do

--- openelis/src/us/mn/state/health/lims/common/formfields/BahmniFormFields.java
+++ openelis/src/us/mn/state/health/lims/common/formfields/BahmniFormFields.java
@@ -55,6 +55,7 @@ public class BahmniFormFields implements IFormFieldsForImplementation {
         settings.put(Field.MothersName, Boolean.FALSE);
         settings.put(Field.SupportPrimaryRelative, Boolean.TRUE);
         settings.put(Field.StNumber, Boolean.TRUE);
+        settings.put(Field.CollectionDate, Boolean.FALSE);
 		return settings;
 	}
 
