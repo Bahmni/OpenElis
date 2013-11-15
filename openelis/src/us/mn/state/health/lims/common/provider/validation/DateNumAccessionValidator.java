f8130aa44bffdd5392360d643fce97b2257f0917
Vinay Venu <vinayv@thoughtworks.com>
2013-11-15 14:12:27 +0530

    Vinay | #0 | Make test independent of order. Minor clean-up.

--- openelis/src/us/mn/state/health/lims/common/provider/validation/DateNumAccessionValidator.java
+++ openelis/src/us/mn/state/health/lims/common/provider/validation/DateNumAccessionValidator.java
@@ -16,26 +16,20 @@
 
 package us.mn.state.health.lims.common.provider.validation;
 
-import org.hibernate.criterion.Restrictions;
 import us.mn.state.health.lims.common.util.SystemConfiguration;
 import us.mn.state.health.lims.common.util.resources.ResourceLocator;
 import us.mn.state.health.lims.sample.dao.SampleDAO;
 import us.mn.state.health.lims.sample.daoimpl.SampleDAOImpl;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.Locale;
 
 public class DateNumAccessionValidator implements IAccessionNumberValidator {
 
     private static String nextAccessionNumber;
     private static final Object LOCK_OBJECT = new Object();
     private SampleDAO sampleDAO = new SampleDAOImpl();
-
-    private enum AccessionNumberState {
-        NEW,
-        CURRENT_HEIGHEST,
-    }
 
     void setSampleDAO(SampleDAO sampleDAO) {
         this.sampleDAO = sampleDAO;
@@ -48,32 +42,6 @@ public class DateNumAccessionValidator implements IAccessionNumberValidator {
 
     @Override
     public ValidationResults validFormat(String accessionNumber, boolean checkDate) throws IllegalArgumentException {
-
-    /*    if (accessionNumber.length() > getMaxAccessionLength() || accessionNumber.length() < getMaxAccessionLength()-1) {
-            return ValidationResults.LENGTH_FAIL;
-        }
-
-        try {
-            SimpleDateFormat format = new SimpleDateFormat("ddMMyyyy");
-            format.setLenient(false);
-            Date datePortion = format.parse(accessionNumber.substring(0, 8));
-            if (datePortion == null) {
-                return ValidationResults.FORMAT_FAIL;
-            }
-        } catch (ParseException e) {
-            return ValidationResults.FORMAT_FAIL;
-        }
-
-        try {
-            Integer.parseInt(accessionNumber.substring(8));
-        } catch (NumberFormatException e) {
-            return ValidationResults.FORMAT_FAIL;
-        }*/
-
-       /* if(!accessionNumber.equals(nextAccessionNumber)){
-            return ValidationResults.FORMAT_FAIL;
-        }
-*/
         return ValidationResults.SUCCESS;
     }
 
@@ -100,24 +68,6 @@ public class DateNumAccessionValidator implements IAccessionNumberValidator {
         int nextIndex = Integer.parseInt(currentHighAccessionNumber.substring(9)) + 1;
         return String.format(currentHighAccessionNumber.substring(0, 9) + "%03d", nextIndex);
     }
-/*
-    @Override
-    public String getNextAvailableAccessionNumber(String programCode) {
-        // This part relevant when the application gets initialized. The following section would always be skipped after the number has been initialized
-        if (nextAccessionNumber == null || hasNextAccessionNumberExpired()) {
-            synchronized (LOCK_OBJECT) {
-                return generateNextAccessionNumber();
-            }
-        }
-        // This part relevant when the application tries to get next accessnumber
-        if (nextAccessionNumber == null || nextAccessionNumber.isEmpty()) {
-            synchronized (LOCK_OBJECT) {
-                nextAccessionNumber = incrementAccessionNumber(nextAccessionNumber);
-            }
-        }
-        return nextAccessionNumber;
-    }*/
-
 
     @Override
     public String getNextAvailableAccessionNumber(String programCode) {
@@ -179,7 +129,6 @@ public class DateNumAccessionValidator implements IAccessionNumberValidator {
     public int getChangeableLength() {
         return 12;
     }
-
 
     private String getDatePrefix() {
         return new SimpleDateFormat("ddMMyyyy").format(new Date()) + "-";
--- openelis/test/us/mn/state/health/lims/common/provider/validation/DateNumAccessionValidatorTest.java
+++ openelis/test/us/mn/state/health/lims/common/provider/validation/DateNumAccessionValidatorTest.java
@@ -16,17 +16,12 @@
 
 package us.mn.state.health.lims.common.provider.validation;
 
-import org.apache.commons.httpclient.util.DateParseException;
-import org.apache.commons.httpclient.util.DateUtil;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import us.mn.state.health.lims.sample.dao.SampleDAO;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
@@ -38,28 +33,23 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class DateNumAccessionValidatorTest {
 
     DateNumAccessionValidator dateNumAccessionValidator;
-    /*******
-     *
-     * Do not change the sequence of Tests
-     *
-     */
 
     @Mock
     SampleDAO sampleDAO;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         initMocks(this);
         dateNumAccessionValidator = new DateNumAccessionValidator();
+        DateNumAccessionValidator.resetAccessionNumber();
     }
 
     @Test
     public void shouldTestNextAccessionNumber() {
-        DateNumAccessionValidator.resetAccessionNumber();
         when(sampleDAO.getLargestAccessionNumberWithPrefix(any(String.class))).thenReturn("02022012-004");
         dateNumAccessionValidator.setSampleDAO(sampleDAO);
         String nextNumber = dateNumAccessionValidator.getNextAvailableAccessionNumber(null);
-        assertEquals("02022012-005",nextNumber);
+        assertEquals("02022012-005", nextNumber);
     }
 
     @Test
@@ -77,29 +67,13 @@ public class DateNumAccessionValidatorTest {
         dateNumAccessionValidator.setSampleDAO(sampleDAO);
         String accNo = getDatePrefix() + "001";
         IAccessionNumberValidator.ValidationResults validAccessionNumber = dateNumAccessionValidator.validFormat(accNo, false);
-        assertEquals(IAccessionNumberValidator.ValidationResults.SUCCESS, validAccessionNumber );
+        assertEquals(IAccessionNumberValidator.ValidationResults.SUCCESS, validAccessionNumber);
     }
 
-    private String getDatePrefix(){
-        return  new SimpleDateFormat("ddMMyyyy").format(new Date()) +"-";
-    }
-
-  /*  @Test
-    public void shouldAssertInvalidAccessionNumber() {
-        IAccessionNumberValidator.ValidationResults validAccessionNumber = dateNumAccessionValidator.validFormat("32032012001", false);
-        assertEquals(IAccessionNumberValidator.ValidationResults.FORMAT_FAIL,validAccessionNumber);
-    }
-
-    @Test
-    public void shouldAssertInvalidNumberPartOfAccessionNumber() {
-        IAccessionNumberValidator.ValidationResults validAccessionNumber = dateNumAccessionValidator.validFormat("02032012a34", false);
-        assertEquals(validAccessionNumber, IAccessionNumberValidator.ValidationResults.FORMAT_FAIL);
-    }
-*/
     @Test
     public void shouldAssertIncrementAssertionNumber() {
         String nextAssertionNumber = dateNumAccessionValidator.incrementAccessionNumber("02022012-004");
-        assertEquals(nextAssertionNumber,"02022012-005");
+        assertEquals(nextAssertionNumber, "02022012-005");
     }
 
     @Test
@@ -108,9 +82,7 @@ public class DateNumAccessionValidatorTest {
         assertTrue(firstAccessionNumber.endsWith("001"));
     }
 
-
-
-
-
-
+    private String getDatePrefix() {
+        return new SimpleDateFormat("ddMMyyyy").format(new Date()) + "-";
+    }
 }
