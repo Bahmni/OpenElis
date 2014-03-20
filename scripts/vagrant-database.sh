d594a86aad3d724430db00a2f49601fec3042d4d
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-20 12:51:43 +0530

    Gurpeet, Balaji| #1965 | Added vagrant-deploy and vagrant-database scripts for devs to deploy stuff to vagrant

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 MACHINE_IP=192.168.33.10
 KEY_FILE=~/.vagrant.d/insecure_private_key
-ssh vagrant@$MACHINE_IP -i $KEY_FILE "sudo su - jss && cd /bahmni_temp/OpenElis/ && ant setupDB"
+ssh vagrant@$MACHINE_IP -i $KEY_FILE "sudo su -c 'cd /bahmni_temp/OpenElis/ && /home/jss/apache-ant-1.9.1/bin/ant setupDB'"
