715a2ade6761b1c8e81aad6887fcb9186d6948a1
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 02:30:33 +0530

    Gurpreet| #1965| Refactored the Vagrant connection into vagrant_functions.sh.

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -x
-MACHINE_IP=192.168.33.10
-KEY_FILE=~/.vagrant.d/insecure_private_key
-ssh vagrant@$MACHINE_IP -i $KEY_FILE "sudo su -c 'cd /bahmni_temp/OpenElis/ && /home/jss/apache-ant-1.9.1/bin/ant setupDB'"
+
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
+
+run_in_vagrant -c "sudo su -c 'cd /bahmni_temp/OpenElis/ && /home/jss/apache-ant-1.9.1/bin/ant setupDB'"
--- scripts/vagrant/vagrant-deploy.sh
+++ scripts/vagrant/vagrant-deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 
+PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $PATH_OF_CURRENT_SCRIPT/vagrant_functions.sh
+
 #All config is here
 TEMP_ELIS_WAR_FOLDER=/tmp/deploy_openelis
-MACHINE_IP=192.168.33.10
 CWD=$1
 SCRIPTS_DIR=$CWD/scripts/vagrant
-KEY_FILE=~/.vagrant.d/insecure_private_key
-TIMEOUT="-o ConnectTimeout=5"
 
 # Setup environment
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/setup_environment.sh
+run_in_vagrant -f "$SCRIPTS_DIR/setup_environment.sh"
 
 # Copy ELIS War file to Vagrant tmp
-scp  -i $KEY_FILE $TIMEOUT $2 vagrant@$MACHINE_IP:$TEMP_ELIS_WAR_FOLDER
+scp_to_vagrant $2 $TEMP_ELIS_WAR_FOLDER
 
 #Deploy them from Vagrant /tmp to appropriate location
-ssh vagrant@$MACHINE_IP -i $KEY_FILE $TIMEOUT < $SCRIPTS_DIR/deploy_elis_war.sh
+run_in_vagrant -f "$SCRIPTS_DIR/deploy_elis_war.sh"
