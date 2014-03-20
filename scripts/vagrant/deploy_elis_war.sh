f5c26a157c8d397d7efebcaa27c249e44e1f5551
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-20 14:57:22 +0530

    Gurpreet, BalajiN | #1965 | Setting user to JSS for copied artifacts in vagrant

--- scripts/vagrant/deploy_elis_war.sh
+++ scripts/vagrant/deploy_elis_war.sh
@@ -4,4 +4,4 @@ TEMP_LOCATION=/tmp/deploy_openelis
 ELIS_WAR_LOCATION=/home/jss/apache-tomcat-7.0.22/webapps
 
 sudo rm -rf $ELIS_WAR_LOCATION/openelis
-sudo cp -f $TEMP_LOCATION/* $ELIS_WAR_LOCATION
+sudo su - jss -c  "cp -f $TEMP_LOCATION/* $ELIS_WAR_LOCATION"
