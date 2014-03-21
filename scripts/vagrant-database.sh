22ae55b958b2ec4556ff6e3c01da7786bfcd7e81
Gurpreet Luthra <gluthra@thoughtworks.com>
2014-03-21 17:35:50 +0530

    Gurpreet| #1965| Modified the vagrant-database.sh script to run against the /Project/OpenELIS ant

--- scripts/vagrant-database.sh
+++ scripts/vagrant-database.sh
@@ -3,4 +3,4 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $PATH_OF_CURRENT_SCRIPT/vagrant/vagrant_functions.sh
 
-run_in_vagrant -c "sudo su -c 'cd /bahmni_temp/OpenElis/ && /home/jss/apache-ant-1.9.1/bin/ant setupDB'"
+run_in_vagrant -c "sudo su -c 'cd /Project/OpenElis/ && /home/jss/apache-ant-1.9.1/bin/ant setupDB'"
