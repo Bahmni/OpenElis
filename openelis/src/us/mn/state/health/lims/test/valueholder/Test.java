2eae6eef3cfead313fe3fccad32b42063e610e76
hemanths <hemanths@Administrators-MacBook-Pro.local>
2016-02-22 19:37:15 +0530

    Revert "Hemanth | #1221 | defaulted is_referred_out property to false."
    This reverts commit ba5ebe103e4de3a9e1cbd2cbc05c74e2e89f87a3.

--- openelis/src/us/mn/state/health/lims/test/valueholder/Test.java
+++ openelis/src/us/mn/state/health/lims/test/valueholder/Test.java
@@ -105,7 +105,7 @@ public class Test extends EnumValueItemImpl {
 
 	private Boolean orderable;
 
-	private Boolean isReferredOut = false;
+	private Boolean isReferredOut;
 
 	private  Boolean isReferredOutValueChanged = false;
 
