27c3877fc6103b043184a69ba9f55bdcd2242021
Ranju Karmakar <ranjukr@in-ranju.local>
2016-05-17 13:00:54 +0530

    Ranju/Gurpreet| Fixed vagrant-deploy script to deploy new location.

--- scripts/vagrant-deploy.sh
+++ scripts/vagrant-deploy.sh
@@ -1,2 +1,18 @@
-#!/bin/sh -x
-ant vagrant-deploy
+PATH_OF_CURRENT_DIRECTORY="$(pwd)"
+BAHMNI_HOME="$PATH_OF_CURRENT_DIRECTORY/../"
+OPENELIS_HOME="$BAHMNI_HOME/OpenElis"
+
+ant setupDB test dist
+mkdir "$OPENELIS_HOME/openelis/dist/openelis"
+cd "$OPENELIS_HOME/openelis/dist/"
+unzip -q openelis.war -d "./openelis/."
+
+cd "$BAHMNI_HOME/bahmni-vagrant/"
+
+vagrant ssh -c "sudo service bahmni-lab stop"
+vagrant ssh -c "sudo rm -rf /opt/bahmni-lab/bahmni-lab/*"
+vagrant ssh -c "sudo cp -r /bahmni/OpenElis/openelis/dist/openelis/* /opt/bahmni-lab/bahmni-lab/."
+vagrant ssh -c "sudo chown -R vagrant:vagrant /opt/bahmni-lab/bahmni-lab/*"
+vagrant ssh -c "sudo service bahmni-lab start"
+
+cd "$PATH_OF_CURRENT_DIRECTORY"
