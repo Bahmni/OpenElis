fa0f8fea437063ca0e7f3e110d2877d0984796a5
Ranju Karmakar <ranjukr@in-ranju.local>
2016-05-17 17:11:47 +0530

    Ranju/Gurpreet | Modified vagrant_function with vagrant ssh, so that password does not require everytime and use more secure private key than insecure key

--- scripts/vagrant-deploy.sh
+++ scripts/vagrant-deploy.sh
@@ -1,4 +1,7 @@
 PATH_OF_CURRENT_DIRECTORY="$(pwd)"
+
+source $PATH_OF_CURRENT_DIRECTORY/scripts/vagrant/vagrant_functions.sh
+
 BAHMNI_HOME="$PATH_OF_CURRENT_DIRECTORY/../"
 OPENELIS_HOME="$BAHMNI_HOME/OpenElis"
 
@@ -9,10 +12,10 @@ unzip -q openelis.war -d "./openelis/."
 
 cd "$BAHMNI_HOME/bahmni-vagrant/"
 
-vagrant ssh -c "sudo service bahmni-lab stop"
-vagrant ssh -c "sudo rm -rf /opt/bahmni-lab/bahmni-lab/*"
-vagrant ssh -c "sudo cp -r /bahmni/OpenElis/openelis/dist/openelis/* /opt/bahmni-lab/bahmni-lab/."
-vagrant ssh -c "sudo chown -R vagrant:vagrant /opt/bahmni-lab/bahmni-lab/*"
-vagrant ssh -c "sudo service bahmni-lab start"
+run_in_vagrant -c "sudo service bahmni-lab stop"
+run_in_vagrant -c "sudo rm -rf /opt/bahmni-lab/bahmni-lab/*"
+run_in_vagrant -c "sudo cp -r /bahmni/OpenElis/openelis/dist/openelis/* /opt/bahmni-lab/bahmni-lab/."
+run_in_vagrant -c "sudo chown -R bahmni:bahmni /opt/bahmni-lab/bahmni-lab/*"
+run_in_vagrant -c "sudo service bahmni-lab start"
 
 cd "$PATH_OF_CURRENT_DIRECTORY"
