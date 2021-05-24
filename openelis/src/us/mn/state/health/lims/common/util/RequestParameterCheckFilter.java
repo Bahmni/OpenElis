46d5b6538fd3619ad30f879e0598fd20331dfc9c
Vivek Singh <petmongrels@gmail.com>
2021-05-24 15:33:15 +0530

    Fix for reflected and stored xss attacks.

---
+++ openelis/src/us/mn/state/health/lims/common/util/RequestParameterCheckFilter.java
@@ -0,0 +1,50 @@
+/*
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations under
+ * the License.
+ *
+ * The Original Code is OpenELIS code.
+ *
+ * Copyright (C) The Minnesota Department of Health.  All Rights Reserved.
+ *
+ * Contributor(s): CIRG, University of Washington, Seattle WA.
+ */
+package us.mn.state.health.lims.common.util;
+
+import org.apache.log4j.Logger;
+import org.owasp.encoder.Encode;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
+public class RequestParameterCheckFilter implements Filter {
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF8");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if (httpServletRequest.getServletPath().endsWith(".do")) {
+            logger.error(String.format("Using safe request: %s", httpServletRequest.getServletPath()));
+            chain.doFilter(new SafeRequest(httpServletRequest), response);
+        }
+        else
+            chain.doFilter(request, response);
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+}
---
+++ openelis/src/us/mn/state/health/lims/common/util/SafeRequest.java
@@ -0,0 +1,48 @@
+package us.mn.state.health.lims.common.util;
+
+import org.apache.log4j.Logger;
+import org.owasp.encoder.Encode;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SafeRequest extends HttpServletRequestWrapper {
+    private final Logger logger = Logger.getLogger(SafeRequest.class);
+
+    public SafeRequest(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String getParameter(String name) {
+        HttpServletRequest request = (HttpServletRequest) this.getRequest();
+        String encodedValue = Encode.forHtml(request.getParameter(name));
+        logger.error(String.format("Intercepted action request: %s, name: %s, value= %s", request.getServletPath(), name, encodedValue));
+        return encodedValue;
+    }
+
+    @Override
+    public Map<String, String[]> getParameterMap() {
+        logger.error("getParameterMap");
+        Map<String, String[]> newParameterMap = new HashMap<>();
+        Map<String, String[]> existingParameterMap = super.getParameterMap();
+        for (String key : existingParameterMap.keySet()) {
+            newParameterMap.put(key, getParameterValues(key));
+        }
+        return newParameterMap;
+    }
+
+    @Override
+    public String[] getParameterValues(String name) {
+        logger.error("getParameterValues");
+        String[] existingValues = super.getParameterValues(name);
+        String[] newValues = new String[existingValues.length];
+        for (int i = 0; i < existingValues.length; i++) {
+            newValues[i] = Encode.forHtml(existingValues[i]);
+        }
+        return newValues;
+    }
+}
