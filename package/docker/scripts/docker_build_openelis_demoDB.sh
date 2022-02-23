89b2827797b3aa7db3c413cd0ba24d8810343339
Abinaya U <abinaya.u@thoughtworks.com>
2022-02-23 18:14:06 +0530

    BAH-1414 | added. separate workflow for build and publish openelis demoDB
    Co-authored-by: Umair Fayaz <omayrfayaz@gmail.com>

--- package/docker/scripts/docker_build_openelis_demoDB.sh
+++ package/docker/scripts/docker_build_openelis_demoDB.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -xe
-#Building bahmni core which has embedded Tomcat Server
-cp core/build/libs/core-1.0-SNAPSHOT.jar package/docker/bahmni-core.jar
-
-# Packaging default config to embed into default image
-cp default_config.zip package/resources/
 
 #Fetching Database Backup Data
 gunzip -f -k bahmni-scripts/demo/db-backups/v0.92/openelis_backup.sql.gz
@@ -18,11 +13,6 @@ mkdir -p build/migrations
 fi
 unzip -u -d build/migrations resources/OpenElis.zip
 
-# Unzipping Default Config
-unzip -q -u -d build/default_config resources/default_config.zip
-
-#Building Docker images
+#Building Docker image
 OPENELIS_IMAGE_TAG=${BAHMNI_VERSION}-${GITHUB_RUN_NUMBER}
-docker build -t bahmni/openelis-db:fresh-${OPENELIS_IMAGE_TAG} -f docker/db.Dockerfile . --no-cache
 docker build -t bahmni/openelis-db:demo-${OPENELIS_IMAGE_TAG} -f docker/demodb.Dockerfile . --no-cache
-docker build -t bahmni/openelis:${OPENELIS_IMAGE_TAG} -f docker/Dockerfile . --no-cache
