059fc81e814ae23778384de74718a75f82bdad55
goyaldivij <divij.g@beehyv.com>
2022-09-14 12:23:37 +0530

    BAH-2276 | Changed bahmni_openelis_revision.json file
    Co-authored-by: MOHANKUMAR T <mohan13081999@gmail.com>

--- package/rpm/test/bahmni_openelis_revision.sh
+++ package/rpm/test/bahmni_openelis_revision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 revision='{
-    "go" : "https://ci-server.mybahmni.org/go/pipelines/value_stream_map/_jobname_/_pipelineCount_",
+    "github_actions" : "https://github.com/Bahmni/OpenElis/actions/runs/GITHUB_RUN_ID/",
     "github": {
         "openelis" : "https://github.com/Bahmni/OpenElis/commit/_sha_"
     }
@@ -12,4 +12,4 @@ replace() {
     sed "s/$1/$envValue/g"
 }
 
-echo $revision | replace "_jobname_" "GO_PIPELINE_NAME" | replace "_pipelineCount_" "GO_PIPELINE_COUNTER" | replace "_sha_" "GO_REVISION_OPENELIS_GITHUB"
+echo $revision | replace "GITHUB_RUN_ID" "GITHUB_RUN_ID" | replace "_sha_" "GITHUB_SHA"
