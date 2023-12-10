f0d113e1500c77884d6888f5a987729a563c48d6
MOHANKUMAR T <mohan13081999@gmail.com>
2023-12-10 19:33:08 +0530

    BAH-2851 | Fix. Unknown field properties error on reference data sync

--- openelis/src/org/bahmni/feed/openelis/feed/contract/bahmnireferencedata/ReferenceDataTestAndPanels.java
+++ openelis/src/org/bahmni/feed/openelis/feed/contract/bahmnireferencedata/ReferenceDataTestAndPanels.java
@@ -3,7 +3,9 @@ package org.bahmni.feed.openelis.feed.contract.bahmnireferencedata;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ReferenceDataTestAndPanels {
 
     private List<ReferenceDataTest> tests;
