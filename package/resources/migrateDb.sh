cffa542a2e4eaa1b42456bae924e648518add24f
MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>
2026-01-07 00:01:45 +0530

    BAH-3394 | Fix. Remove hardcoded credentials in DB Migration script (#98)

--- package/resources/migrateDb.sh
+++ package/resources/migrateDb.sh
@@ -8,4 +8,4 @@ if [ -f /etc/bahmni-installer/bahmni.conf ]; then
 . /etc/bahmni-installer/bahmni.conf
 fi
 
-java -jar -Dfile.encoding=utf-8 lib/liquibase-1.9.5.jar --defaultsFile=liquibase.properties --contexts=bahmni --url=jdbc:postgresql://$OPENELIS_DB_SERVER:5432/clinlims update
+java -jar -Dfile.encoding=utf-8 lib/liquibase-1.9.5.jar --defaultsFile=liquibase.properties --contexts=bahmni --defaultSchemaName=clinlims --url=jdbc:postgresql://$OPENELIS_DB_SERVER:$OPENELIS_DB_PORT/$OPENELIS_DB_NAME --username=$OPENELIS_DB_USERNAME --password=$OPENELIS_DB_PASSWORD update
